e71a88f6a87c8d1ea7b7678ed400211dee3f77b4
Andreas Vohns <40440303+avohns@users.noreply.github.com>
2020-06-24 08:36:57 +0200

    Add files via upload

---
+++ simple-examples-de/beispiel1_arithmetik1.py
@@ -0,0 +1,28 @@
+for x in range(2,10):
+  for y in range(2,10):
+    if x > y:
+      if x/y == x//y:
+        points=4
+      else:
+        points=3
+      print(rf"""\begin{{cloze}}[points={points}]{{Arithmetik-Quiz {(x,y)}}}
+      L\"ose die folgenden Aufgaben!\\
+      \begin{{numerical}}
+      ${x} + {y} =$
+      \item {x+y}
+      \end{{numerical}}\\
+      \begin{{numerical}}
+      ${x} - {y} =$
+      \item {x-y}
+      \end{{numerical}}\\
+      \begin{{numerical}}
+      ${x} \cdot {y} =$
+      \item {x*y}
+      \end{{numerical}}\\""")
+      if x/y == x//y:
+        print(rf"""\begin{{numerical}}
+        ${x} : {y} =$
+        \item {x//y}
+        \end{{numerical}}\\""")
+      print(rf"\end{{cloze}}")
+
